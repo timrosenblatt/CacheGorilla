2c4f5376a2606a6c4d4db4aed54d462acdd747dc
Tim Rosenblatt <tim@cloudspace.com>
2010-07-10 22:35:54 -0400

    added memcache; improved testing; updated description with smartass comment

--- lib/CacheGorilla.rb
+++ lib/CacheGorilla.rb
@@ -28,27 +28,42 @@ module CacheGorilla
 
   # This code is heavily inspired by Yehuda's Moneta (http://github.com/wycats/moneta)
   class CacheGorilla
+    # :server sets up memcache, :host sets up mongo
     def initialize(options = {})
-      options = {
+      @options = {
         :host => ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost',
         :port => ENV['MONGO_RUBY_DRIVER_PORT'] || 27017,
         :db => 'cache',
-        :collection => 'cache'
+        :collection => 'cache',
+        :server => 'localhost'
       }.update(options)
 
-      @mongo_connection = Mongo::Connection.new("localhost", 27017, :pool_size => 5, :timeout => 5)
-
-      @mongo_collection = @mongo_connection.db(options[:db]).collection(options[:collection])
+      @mongo_connection = Mongo::Connection.new(@options[:host], @options[:port], :pool_size => 5, :timeout => 5)
+      @mongo_collection = @mongo_connection.db(@options[:db]).collection(@options[:collection])
+      
+      @memcache = MemCache.new(options[:server], @options)
     end
 
     def key?(key)
       !!self[key]
     end
+    
+    alias has_key? key?
 
-    def [](key)
-      res = @mongo_collection.find({'_id' => key}).first
-      res = nil if res && res['expires'] && Time.now > res['expires']
-      res && res['data']
+    def [](key)      
+      begin
+        @memcache.get(key)
+      rescue Memcached::NotFound
+        res = @mongo_collection.find({'_id' => key}).first
+        res = nil if res && res['expires'] && Time.now > res['expires']
+        
+        if res
+          args = [res['_id'], res['data'], res['expires']].compact
+          @memcache.set(*args)
+        end
+        
+        res && res['data']
+      end
     end
 
     def []=(key, value)
@@ -56,28 +71,30 @@ module CacheGorilla
     end
 
     def delete(key)
+      # todo What's the best way to run these two calls at once, given that it's very much one-at-a-time
       value = self[key]
       @mongo_collection.remove('_id' => key) if value
+      @memcache.delete(key) if value
+      value
+    end
+    
+    # Pass an option of :bypass_memcache if you want. Set the key to any value.
+    def store(key, value, options = {})
+      # todo What's the best way to run these two calls at once, given that it's very much one-at-a-time
+      exp = options[:expires_in] ? (Time.now + options[:expires_in]) : nil
+      @mongo_collection.update({ '_id' => key }, { '_id' => key, 'data' => value, 'expires' => exp }, { :upsert => true }) # upsert is the best technical term ever.
+      
+      unless options.has_key?(:bypass_memcache)
+        args = [key, value, options[:expires_in]].compact
+        @memcache.set(*args)
+      end
+      
       value
     end
 
-    def store(key, value, options = {})
-      exp = options[:expires_in] ? (Time.now + options[:expires_in]) : nil
-      @mongo_collection.update({ '_id' => key }, { '_id' => key, 'data' => value, 'expires' => exp }, { :upsert => true }) # upsert is the best technical term ever.
-    end
-
-    # Not sure this is useful anymore, since :upsert is being used in #store
-    def update_key(key, options = {})
-      val = self[key]
-      self.store(key, val, options)
-    end
-
     def clear
-      @mongo_collection.clear
-    end
-    
-    def drop_database(name)
-      @mongo_connection.drop_database(name)
+      @mongo_connection.drop_database(@options[:db])
+      @memcache.flush
     end
   end
 end
--- spec/CacheGorilla_spec.rb
+++ spec/CacheGorilla_spec.rb
@@ -1,5 +1,31 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
+# For sneaking a peek during testing
+module CacheGorilla
+  class CacheGorilla
+    # attr_accessor :memcache, :mongo_collection
+  
+    def mongo_get(key)
+      @mongo_collection.find({'_id' => key}).first['data']
+    end
+  
+    def mongo_set(key, value)
+      @mongo_collection.update({ '_id' => key }, { '_id' => key, 'data' => value, 'expires' => nil }, { :upsert => true })
+    end
+  
+    def memcache_get(key)
+      @memcache.get(key)
+    rescue Memcached::NotFound
+      nil
+    end
+  
+    def memcache_set(key, value)
+      args = [key, value]
+      @memcache.set(*args)
+    end
+  end
+end
+
 describe "CacheGorilla" do
   include CacheGorilla
   
@@ -8,25 +34,56 @@ describe "CacheGorilla" do
   end
   
   after(:each) do
-    @cg.drop_database("cache_gorilla_test")
+    @cg.clear
   end
   
   it "can set and get values" do
     @cg.key?("Unicorns!").should be_false
+    
     @cg["Unicorns!"] = "Ponies!"
+    
     @cg["Unicorns!"].should == "Ponies!"
   end
   
   it "can check for the presence of a key" do
-    @cg.key?("Unicorns!").should be_false
-    @cg["Unicorns!"] = "Ponies!"
-    @cg.key?("Unicorns!").should be_true
+    @cg.key?("Scrooge").should be_false
+    
+    @cg["Scrooge"] = "Christmas Spirit"
+    
+    @cg.key?("Scrooge").should be_true
   end
   
   it "can delete keys" do
     @cg["Unicorns!"] = "Ponies!"
     @cg.delete("Unicorns!")
+    
     @cg.key?("Unicorns!").should be_false
+  end
+  
+  it "respects expirations" do
+    @cg.store("key", "value", { :expires_in => 5 })
+    sleep(6)
+    @cg["key"].should be_nil
+  end
+  
+  it "fills memcache on cache misses" do
+    @cg.memcache_get("key").should be_nil
+    @cg.mongo_set("key", "value")
+    
+    @cg["key"].should == "value"
+    
+    @cg.memcache_get("key").should == "value"
+  end
+  
+  it "returns nil when a key is not found" do
+    @cg["EasterBunny"].should be_nil
+  end
+  
+  it "sets values in both memcache and mongo" do
+    @cg["key"] = "value"
+    
+    @cg.mongo_get("key").should == "value"
+    @cg.memcache_get("key").should == "value"
   end
   
 end
